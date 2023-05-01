1751440e262cbb897a6150a826a4ef919906aab8
Chukwuka419 <coolchuka5@gmail.com>
2023-05-01 23:40:41 +0100

    before Query

--- src/component/Services.jsx
+++ src/component/Services.jsx
@@ -1,25 +1,20 @@
-import React from 'react'
-import '../style/services.css'
-import Myxen from '../img/Myxen.png'
-import Zyris from '../img/Zyris.png'
-import Nylarion from '../img/Nylarion.png'
-import ImgBox from './ImgBox'
-
-
+import React from "react";
+import "../style/services.css";
+import Myxen from "../img/Myxen.png";
+import Zyris from "../img/Zyris.png";
+import Nylarion from "../img/Nylarion.png";
+import ImgBox from "./ImgBox";
 
 const Services = () => {
   return (
-    <section className='service_wapper'>
-        <h1> Services Stanley Realtor Provides</h1>
-        <div className='service'>
-
-       <ImgBox img={Myxen} title="Investment"/>
-       <ImgBox img={Nylarion} title="Brokerage"/>
-       <ImgBox img={Zyris} title="Management"/>
-       </div>
-
+    <section className="service_wapper">
+      <h1> Services Stanley Realtor Provides</h1>
+      <div className="service">
+        <ImgBox img={Myxen} title="Investment" />
+        <ImgBox img={Nylarion} title="Brokerage" />
+        <ImgBox img={Zyris} title="Management" />
+      </div>
     </section>
-  )
-}
-
+  );
+};
 export default Services;
